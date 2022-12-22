f079512b13a993ad9eb58163fecd3e0d43b1e1b6
Kemps Medeiros <kempsmm@gmail.com>
2022-12-22 09:36:51 -0300

    first commit

---
+++ app/routes/jokes.tsx
@@ -0,0 +1,19 @@
+import { LinksFunction } from "@remix-run/node";
+import { Outlet } from "@remix-run/react";
+
+import stylesUrl from "~/styles/jokes.css";
+
+export const links: LinksFunction = () => {
+    return [{ rel: "stylesheet", href: stylesUrl }];
+  };
+
+export default function JokesRoute() {
+    return (
+        <div>
+            <h1>Jokes</h1>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+}
